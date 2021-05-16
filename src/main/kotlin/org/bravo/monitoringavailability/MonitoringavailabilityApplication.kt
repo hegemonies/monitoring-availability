31d45a9fb67fb97db586940abd5d6a5e05d76bb6
Daniil Dyachenko <hegemonies@yandex.ru>
2021-05-16 19:20:26 +0700

    Init project

---
+++ src/main/kotlin/org/bravo/monitoringavailability/MonitoringavailabilityApplication.kt
@@ -0,0 +1,11 @@
+package org.bravo.monitoringavailability
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonitoringavailabilityApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonitoringavailabilityApplication>(*args)
+}
---
+++ src/test/kotlin/org/bravo/monitoringavailability/MonitoringavailabilityApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bravo.monitoringavailability
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonitoringavailabilityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
